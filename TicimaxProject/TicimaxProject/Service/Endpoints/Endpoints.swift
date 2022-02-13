e121681710c3e3b0fe4447721de08aa2415fa4d5
Ali Akgün <ali.akgun@appcent.mobi>
2022-02-13 20:23:16 +0300

    [Ticimax-Project]
    - pods added.

---
+++ TicimaxProject/TicimaxProject/Service/Endpoints/Endpoints.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoints.swift
+//  TicimaxProject
+//
+//  Created by Ali Akgün on 13.02.2022.
+//
+
+import Foundation
+import Foundation
+
+struct Endpoints {
+//    static let search = "/search"
+}
